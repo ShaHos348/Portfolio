89cb4227e101a8c4f59a09864239cd6cb22c030e
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2024-05-04 23:48:57 -0400

    Fixed navbar and made first version of about-me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,16 @@ import pfp from "../assets/pfp.jpg";
 
 const About = () => {
   return (
-    <div id="about me">
+    <div id="about-me">
       <h1>About Me</h1>
-      <p>
-        
-      </p>
-      <img src={pfp} alt="Profile" />
+      <section>
+        <p>
+          Hello, My name is Shahbin (Sha) Hossain. I am a CS Major @ GaTech. I
+          have done full-stack web-development as well as game development in
+          the past. Welcome to my portfolio website.
+        </p>
+        <img src={pfp} alt="Profile" />
+      </section>
     </div>
   );
 };
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,30 +1,36 @@
 import * as React from "react";
+import { useRef } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import "./styles/NavBar.css";
 
 const pages = ["About Me", "Experience", "Projects", "Contact Me"];
 
 function NavBar() {
+  const appBarRef = useRef(null);
+
   const navigateToPage = (page) => {
-    const section = document.getElementById(page.toLowerCase());
+    const section = document.getElementById(page.toLowerCase().replace(/\s+/g, '-'));
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
+      const navbarHeight = appBarRef.current.offsetHeight; 
+      const sectionPosition = section.offsetTop - navbarHeight; 
+      window.scrollTo({ top: sectionPosition, behavior: 'smooth' }); 
     }
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+    <AppBar ref={appBarRef} className="app-bar"> 
+      <Container className="container"> 
+        <Toolbar className="toolbar">
+          <Box className="box">
             {pages.map((page, index) => (
               <Button
                 key={index}
                 onClick={() => navigateToPage(page)}
-                sx={{ my: 2, color: "white", display: "block" }}
+                className="button" 
               >
                 {page}
               </Button>
@@ -35,5 +41,6 @@ function NavBar() {
     </AppBar>
   );
 }
+
 
 export default NavBar;
