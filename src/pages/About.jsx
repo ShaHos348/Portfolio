4838b6fa7bec23b62cd21495ff785af2480555a8
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2024-05-06 17:50:38 -0400

    Added button for resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,23 @@
 import "./styles/About.css";
 import pfp from "../assets/pfp.jpg";
+import resume from "./data/Shahbin_Hossain_s_Resume.pdf";
 
 const About = () => {
+
   return (
     <div id="about-me">
       <h1 className="section-header">About Me</h1>
-      <section id="about-section">
-        <p id="about-p">
-          Hello, My name is Shahbin (Sha) Hossain. I am a CS Major @ GaTech. I
-          have done full-stack web-development as well as game development in
-          the past. Welcome to my portfolio website.
-        </p>
+      <div id="about-section">
+        <div id="about-left-column">
+          <p id="about-p">
+            Hello, My name is Shahbin (Sha) Hossain. I am a CS Major @ GaTech. 
+            I have done both web-development as well as game development in the past. 
+            Welcome to my portfolio website.
+          </p>
+          <a id="resume-button" href = {resume} target = "_blank"  rel="noopener noreferrer">Resume</a>
+        </div>
         <img id="about-img" src={pfp} alt="Profile" />
-      </section>
+      </div>
     </div>
   );
 };
