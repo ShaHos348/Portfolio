e571e892a5e33eb10115bb1bd33cbb3028f68008
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2024-05-06 16:24:46 -0400

    Created Contact Section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import "./styles/Contact.css";
+import contactData from "./data/contactData";
 
 const Contact = () => {
+  const handleOnClick = (link) => {
+    window.location.href = link;
+  };
+
   return (
     <div id="contact-me">
       <h1 className="section-header">Contact Me</h1>
+      <div className="contact-container">
+        {contactData.map((contact, index) => (
+          <div
+            className="contact"
+            key={index}
+            onClick={() => handleOnClick(contact.link)}
+          >
+            <img className="contact-icon" src={contact.image} alt="" />
+            <p className="contact-name">{contact.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/data/contactData.js
@@ -0,0 +1,26 @@
+import GitHubIcon from '../../assets/mediaIcons/GitHubIcon.png';
+import LinkedInIcon from '../../assets/mediaIcons/LinkedInIcon.png';
+import GmailIcon from '../../assets/mediaIcons/GmailIcon.png';
+
+let contactData = [
+    {
+      id: 1,
+      name: "GitHub",
+      link: "https://github.com/ShaHos348",
+      image: GitHubIcon,
+    },
+    {
+      id: 2,
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/shahbin-hossain/",
+      image: LinkedInIcon,
+    },
+    {
+      id: 3,
+      name: "Gmail",
+      link: "mailto:shahbinalve@gmail.com",
+      image: GmailIcon,
+    }
+  ];
+  
+  export default contactData;
