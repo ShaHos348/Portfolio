fea9d3c51f1bb043ae97898cb23cca7b77f87ef2
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2024-05-06 18:21:31 -0400

    Messed with css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,23 @@
 import "./styles/About.css";
 import pfp from "../assets/pfp.jpg";
 import resume from "./data/Shahbin_Hossain_s_Resume.pdf";
+import React, { useState, useEffect } from 'react';
 
 const About = () => {
+  const [componentHeight, setComponentHeight] = useState(0);
+
+  useEffect(() => {
+    const component1 = document.querySelector('#nav-app-bar');
+    const component2 = document.querySelector('.section-header');
+    if (component1 && component2) {
+      setComponentHeight(component1.offsetHeight + component2.offsetHeight);
+    }
+  }, []);
 
   return (
     <div id="about-me">
       <h1 className="section-header">About Me</h1>
-      <div id="about-section">
+      <div id="about-section" style={{ height: `calc(100vh - ${componentHeight}px)` }}>
         <div id="about-left-column">
           <p id="about-p">
             Hello, My name is Shahbin (Sha) Hossain. I am a CS Major @ GaTech. 
